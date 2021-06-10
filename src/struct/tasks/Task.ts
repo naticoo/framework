276d0f3bc9c9d27fdf23a6379aa6c8a3486e84c2
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-10 21:59:00 +0200

    fix broken import

--- src/struct/tasks/Task.ts
+++ src/struct/tasks/Task.ts
@@ -1,18 +1,18 @@
-import { NaticoTaskHandler } from "./NaticoTaskHandler";
-import { NaticoModule } from "../NaticoModule.ts";
+import { NaticoTaskHandler } from './TaskHandler.ts';
+import { NaticoModule } from '../NaticoModule.ts';
 export class NaticoTask extends NaticoModule {
-  declare handler: NaticoTaskHandler;
-  delay?: number;
-  runOnStart?: boolean;
-  constructor(
-    id: string,
-    { delay, runOnStart = false }: { delay?: number; runOnStart?: boolean },
-  ) {
-    super(id);
-    this.delay = delay;
-    this.runOnStart = runOnStart;
-  }
-  exec() {
-    throw new Error(`${this.id} no implementated`);
-  }
+	declare handler: NaticoTaskHandler;
+	delay?: number;
+	runOnStart?: boolean;
+	constructor(
+		id: string,
+		{ delay, runOnStart = false }: { delay?: number; runOnStart?: boolean }
+	) {
+		super(id);
+		this.delay = delay;
+		this.runOnStart = runOnStart;
+	}
+	exec() {
+		throw new Error(`${this.id} no implementated`);
+	}
 }
