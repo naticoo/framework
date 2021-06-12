9a2b94546e16ffe211dcbd6269eb2f6336be596e
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-12 20:28:27 +0200

    remove(test.ts)

--- test.ts
+++
@@ -1,8 +0,0 @@
-class Test {
-  public bb = "test";
-}
-const test = new Test();
-let fake = test;
-let clone = Object.assign(Object.create(Object.getPrototypeOf(test)), test);
-clone.bb = "ae";
-console.log(test, clone);
