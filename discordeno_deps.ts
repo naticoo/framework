9e68fb3e6f0903408c7647d0e7572cc300d3cfd7
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-29 15:21:43 +0200

    feat(deps): switch to discordeno main branch

--- discordeno_deps.ts
+++ discordeno_deps.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/discordeno@11.2.0/mod.ts";
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/a0087edecbb0a88eee0c76c27b71ca3b661f3cdf/mod.ts";
