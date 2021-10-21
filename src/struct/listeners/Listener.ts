b8ba04994173438b4c1c6d7a589212f39b3c76b3
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2021-10-21 19:48:43 +0200

    feat(listener handler): Add typed events

--- src/struct/listeners/Listener.ts
+++ src/struct/listeners/Listener.ts
@@ -1,10 +1,13 @@
 import { NaticoModule, NaticoModuleOptions } from "../NaticoModule.ts";
 import { NaticoListenerHandler } from "./ListenerHandler.ts";
+import { EventHandlersDefinitions } from "../../../discordeno_deps.ts";
 
-export interface NaticoListenerOptions extends NaticoModuleOptions {
-  event: string;
+export interface NaticoListenerOptions<T = any> extends NaticoModuleOptions {
+  event: T;
   emitter: string;
 }
+
+export type NaticoListenerOptionsDiscordenoEvents = NaticoListenerOptions<keyof EventHandlersDefinitions>;
 
 export abstract class NaticoListener extends NaticoModule {
   declare handler: NaticoListenerHandler;
